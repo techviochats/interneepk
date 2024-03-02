ea97595d925433a63b9a6c3d2ea3d2d525eba5a7
techviochats <techviochat@gmail.com>
2024-03-02 22:49:37 +0500

    add

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import HeroBanner from "@/components/header/hero-banner";
 import IntroSection from "@/components/intro-section";
 import Testimonials from "@/components/testimonials";
-
+export const runtime = "edge";
 export default function Home() {
   return (
     <>
